2a8a5ab112978afcad4ab578c3c560899b9caaf2
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-06-26 18:27:13 +0530

    commit all changes

---
+++ Lists/basic_operation.py
@@ -0,0 +1,15 @@
+# Program: Basic operations on list 
+numbers = [10, 20, 30, 40, 50]
+
+# Assessing the list elements
+print("List:", numbers)
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+# Inserting element
+numbers[2] = 99
+print("After update:", numbers)
+
+# Printing list element 
+print("Slice:", numbers[1:4])
+print("Length:", len(numbers))
