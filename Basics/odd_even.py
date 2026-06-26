b1447d7d5ddc67adbd4b462670df36ec656bfbe7
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-06-26 18:37:28 +0530

    commit all changes

---
+++ Basics/odd_even.py
@@ -0,0 +1,11 @@
+# Program to check if a number is odd or even
+
+# Taking input from user
+number = int(input("Enter any number: "))
+
+# Checking even or odd using modulus
+if number % 2 == 0:
+    print("Number is Even")
+else:
+    print("Number is Odd")
+  
