6d29a530eb24bda2da626ee72c1e8c4913800603
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-07-01 16:41:17 +0530

    Add student data storage using a tuple

---
+++ Tuple/student_tuple.py
@@ -0,0 +1,10 @@
+# Program to store student data using tuple
+
+student = ("Anjali", 18, "CSE", 95)
+
+print("Student Details:")
+
+print("Name:", student[0])
+print("Age:", student[1])
+print("Branch:", student[2])
+print("Marks:", student[3])
