31665eeb40859201a89ed87c59f931de343c6732
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-07-01 16:40:34 +0530

    Add basic tuple operations demonstration

---
+++ Tuple/basic_tuple_operations.py
@@ -0,0 +1,15 @@
+# Program to demonstrate basic tuple operations
+
+numbers = (10, 20, 30, 40, 50)
+
+print("Tuple:", numbers)
+
+# Accessing elements
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+# Slicing
+print("Sliced tuple:", numbers[1:4])
+
+# Length of tuple
+print("Length:", len(numbers))
