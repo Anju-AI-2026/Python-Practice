cb3a8e9d33e20ed467972c737bc968f4639184a8
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-07-02 18:16:36 +0530

    Add merge_tuples.py to merge two tuples

---
+++ Tuple/merge_tuples.py
@@ -0,0 +1,10 @@
+# Program to merge two tuples
+
+tuple1 = (10, 20, 30)
+tuple2 = (40, 50, 60)
+
+merged_tuple = tuple1 + tuple2
+
+print("First Tuple :", tuple1)
+print("Second Tuple:", tuple2)
+print("Merged Tuple:", merged_tuple)
