685cbfd05538fb0e0da68b7e2bbeccb33a3398bf
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-07-04 16:45:00 +0530

    Create basic_string_operations.py

---
+++ Strings/basic_string_operations.py
@@ -0,0 +1,11 @@
+# Program to demonstrate basic string operations
+
+text = input("Enter a string: ")
+
+# Display basic information about the string
+print("\nOriginal String :", text)
+print("Length          :", len(text))
+print("First Character :", text[0])
+print("Last Character  :", text[-1])
+print("First 5 Characters:", text[:5])
+print("Last 5 Characters :", text[-5:])
