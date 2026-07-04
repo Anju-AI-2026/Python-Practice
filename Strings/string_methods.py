4028c514b0b1fe8eb06ab9659f2bb8f691feff41
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-07-04 16:53:04 +0530

    Add string_methods.py to showcase string methods
    Demonstrates common string methods in Python.

---
+++ Strings/string_methods.py
@@ -0,0 +1,11 @@
+# Program to demonstrate common string methods
+
+text = input("Enter a string: ")
+
+# Display the result of different string methods
+print("\nUppercase      :", text.upper())
+print("Lowercase      :", text.lower())
+print("Title Case     :", text.title())
+print("Capitalized    :", text.capitalize())
+print("Swap Case      :", text.swapcase())
+print("Without Spaces :", text.strip())
