f754ea5f9c64946eecb8193a44a1e5ccdd63e0c5
Anjali Sangannavar <sangannavaranjali@gmail.com>
2026-07-10 19:42:05 +0530

    Update vending_machine_simulator.py

--- Mini Projects/vending_machine_simulator.py
+++ Mini Projects/vending_machine_simulator.py
@@ -14,18 +14,26 @@ while True:
     if choice == "1":
         print("\nYou selected Chips")
         print("Price : ₹20")
+        item = "Chips"
+        price = 20
 
     elif choice == "2":
         print("\nYou selected Chocolate")
         print("Price : ₹30")
+        item ="Chocolate"
+        price = 30
 
     elif choice == "3":
         print("\nYou selected Juice")
         print("Price : ₹40")
+        item = "Juice"
+        price = 40
 
     elif choice == "4":
         print("\nYou selected Water")
         print("Price : ₹15")
+        item = "Water"
+        price = 15
 
     elif choice == "5":
         print("Thank you for using the Vending Machine.")
@@ -33,3 +41,13 @@ while True:
 
     else:
         print("Invalid choice. Please try again.")
+
+    quantity = int(input("Enter quantity: "))
+
+    total = price * quantity
+
+    print("\n===== BILL =====")
+    print("Item :", item)
+    print("Price :", price)
+    print("Quantity :", quantity)
+    print("Total Bill : ₹", total)   
